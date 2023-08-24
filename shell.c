3d72b04bb7893ea98c786a8fbfb5275879b1c26c
Wamalwa Nelson <83448279+nelsonwamalwa@users.noreply.github.com>
2023-08-24 06:00:04 +0300

    Last files

--- shell.c
+++ shell.c
@@ -1,90 +1,89 @@
 #include "shell.h"
 
-#define MAX_COMMAND_LENGTH 100
-#define MAX_ARGS 20
-
-/* Display the shell prompt */
-void displayPrompt() {
-    printf("Shell> ");
-    fflush(stdout);
+/**
+ * _getline - Wrapper function for the getline function.
+ * @line: Pointer to the buffer that will store the line.
+ * @n: Pointer to the size of the buffer.
+ * @stream: Pointer to the file stream.
+ * 
+ * Return: The number of characters read, or -1 on failure.
+ */
+ssize_t _getline(char **line, size_t *n, FILE *stream)
+{
+    return getline(line, n, stream);
 }
 
-int main() {
-    char *command = NULL;
-    size_t commandSize = 0;
-    
+/**
+ * parse_input - Parse input into individual arguments.
+ * @input: Input string to be parsed.
+ * @args: Array to store parsed arguments.
+ * 
+ * Return: Number of arguments parsed.
+ */
+int parse_input(char *input, char **args)
+{
+    int argc = 0;
+    char *token = strtok(input, " \t\n");
+    while (token != NULL) {
+        args[argc++] = token;
+        token = strtok(NULL, " \t\n");
+    }
+    args[argc] = NULL;
+    return argc;
+}
+
+/**
+ * main - Entry point of the custom shell.
+ * 
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
     while (1) {
-        ssize_t bytesRead;
-        pid_t childPid;
+        printf("CustomShell$ ");
+        fflush(stdout);
 
-        int argCount = 0;
-        char *args[MAX_ARGS];
-        char *token;
-
-        displayPrompt();
-        
-        /* Read user input using getline */
-        bytesRead = getline(&command, &commandSize, stdin);
-        
-        if (bytesRead == -1) {
-            if (feof(stdin)) {
-                /* Handle end of file (Ctrl+D) */
-                printf("\nExiting shell.\n");
-                break;
-            } else {
-                perror("Input error");
-                clearerr(stdin);  /* Clear the error state */
-                continue;         /* Continue to the next iteration */
-            }
+        read = _getline(&line, &len, stdin);
+        if (read == -1) {
+            break;
         }
-        
-        /* Remove the newline character from the command */
-        if (command[bytesRead - 1] == '\n') {
-            command[bytesRead - 1] = '\0';
-        }
-        
-        /* Tokenize the command */
-        token = strtok(command, " ");
-        
-        while (token != NULL && argCount < MAX_ARGS - 1) {
-            args[argCount++] = token;
-            token = strtok(NULL, " ");
-        }
-        args[argCount] = NULL;
-        
-        /* Fork a child process */
-        childPid = fork();
 
-        if (childPid < 0) {
-            /* Error forking */
-            perror("Fork error");
-        } else if (childPid == 0) {
-            /* Child process */
-            /* Execute the command using execvp */
-            execvp(args[0], args);
+        if (read > 1) { 
+            char *args[MAX_ARGS];
+            int argc = parse_input(line, args);
 
-            /* If execvp returns, an error occurred */
-            perror("Command execution error");
-            exit(EXIT_FAILURE);
-        } else {
-            /* Parent process */
-            /* Wait for the child process to complete */
-            int childStatus;
-            waitpid(childPid, &childStatus, 0);
-
-            /* Check if the child process exited normally or due to a signal */
-            if (WIFEXITED(childStatus)) {
-                int exitStatus = WEXITSTATUS(childStatus);
-                printf("Child process exited with status %d\n", exitStatus);
-            } else if (WIFSIGNALED(childStatus)) {
-                int terminatingSignal = WTERMSIG(childStatus);
-                printf("Child process was terminated by signal %d\n", terminatingSignal);
+            if (argc > 0) {
+                if (strcmp(args[0], "cd") == 0) {
+                    if (argc != 2) {
+                        fprintf(stderr, "cd: Usage: cd <directory>\n");
+                    } else {
+                        if (chdir(args[1]) != 0) {
+                            perror("cd");
+                        }
+                    }
+                } else if (strcmp(args[0], "exit") == 0) {
+                    exit(0);
+                } else {
+                    pid_t pid = fork();
+                    if (pid == 0) {
+                        execvp(args[0], args);
+                        perror("execvp");
+                        exit(1);
+                    } else if (pid > 0) {
+                        int status;
+                        waitpid(pid, &status, 0);
+                    } else {
+                        perror("fork");
+                    }
+                }
             }
         }
     }
-    
-    /* Free the allocated memory for the command */
-    free(command);
-    
-    return 0;
+
+    free(line);
+    return (0);
 }
