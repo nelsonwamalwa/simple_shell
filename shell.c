4077b0094b8210f512db6b2316d9a79e3b73da99
abel <abelamare3022@gmail.com>
2023-08-26 18:54:57 +0300

    valgrind fix?

--- shell.c
+++ shell.c
@@ -16,95 +16,96 @@ void displayPrompt(void)
 }
 
 /**
- * getCommand - Reads a line from standard input into the provided buffer.
+ * parse_input - Parse input into individual arguments.
+ * @input: Input string to be parsed.
+ * @args: Array to store parsed arguments.
  *
- * @command: Pointer to the buffer where the command will be stored.
- * @commandSize: Pointer to the size of the command buffer.
- * Return: The number of characters read, or -1 on failure.
+ * Return: Number of arguments parsed.
  */
-ssize_t getCommand(char **command, size_t *commandSize)
-{
-	return (getline(command, commandSize, stdin));
-}
+ int main(void)
+ {
+    char *command = NULL;
+    size_t commandSize = 0;
 
-/**
- * executeCommand - Executes a command using the execvp system call.
- *
- * @args: Null-terminated array of command arguments.
- * Return: This function does not return on success, or exits with an error mes
- * sage on failure.
- */
-void executeCommand(char **args)
-{
-	execvp(args[0], args);
-	perror("Command execution error");
-	exit(EXIT_FAILURE);
-}
+    while (1)
+    {
+        ssize_t bytesRead;
+        pid_t childPid;
 
-/**
- * waitForChildProcess - Waits for a child process to complete.
- *
- * @childPid: The process ID of the child process to wait for.
- * Return: None
- */
-void waitForChildProcess(pid_t childPid)
-{
-	int childStatus;
+        int argCount = 0;
+        char *args[MAX_ARGS];
+        char *token;
 
-	waitpid(childPid, &childStatus, 0);
-	if (WIFEXITED(childStatus))
+        displayPrompt();
+
+        /* Read user input using getline */
+        bytesRead = getline(&command, &commandSize, stdin);
+
+	if (bytesRead == -1)
 	{
-		(void)WEXITSTATUS(childStatus);
-	}
-}
-
-/**
- * main - Entry point of the program.
- *
- * This is the main function where the program's execution begins. It serves as
- * the
- * entry point for the program's logic and control flow. Within this function,
- * you can
- * call other functions, perform initialization, and manage the overall executi
- * on of
- * the program.
- *
- * @void: This function accepts no arguments.
- * Return: The exit status of the program. Typically, 0 indicates successful
- * execution,and non-zero values are used to indicate errors or
- * abnormal termination.
- */
-int main(void)
-{
-	char *command = NULL;
-	size_t commandSize = 0;
-
-	while (1)
-	{
-		ssize_t bytesRead;
-		/* pid_t childPid;*/
-		/* char *args[MAX_ARGS];*/
-
-		/*char token and the other one*/
-		displayPrompt();
-		bytesRead = getCommand(&command, &commandSize);
-		if (bytesRead == -1)
+		if (feof(stdin))
 		{
-			if (feof(stdin))
-				break;
+			/* Handle end of file (Ctrl+D) */
+			break;
+		}
+		else
+		{
 			perror("Input error");
-			clearerr(stdin);
-			continue;
-		}
-		if (command[bytesRead - 1] == '\n')
-		{
-			command[bytesRead - 1] = '\0';
-		}
-		/* token = strtok(command, " ");*/
-		/*delete one line below*/
-		/**here*/
-	}
-	free(command);
+			clearerr(stdin);/* Clear the error state */
+			
+			free(command);
 
-	return (0);
+			continue;/* Continue to the next iteration */
+		}
+	}
+        /* Remove the newline character from the command */
+        if (command[bytesRead - 1] == '\n') {
+            command[bytesRead - 1] = '\0';
+        }
+
+        /* Tokenize the command */
+        token = strtok(command, " ");
+
+        while (token != NULL && argCount < MAX_ARGS - 1) {
+            args[argCount++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[argCount] = NULL;
+
+        /* Fork a child process */
+        childPid = fork();
+
+        if (childPid < 0) {
+            /* Error forking */
+            perror("Fork error");
+        } else if (childPid == 0) {
+            /* Child process */
+            /* Execute the command using execvp */
+            execvp(args[0], args);
+
+            /* If execvp returns, an error occurred */
+            perror("Command execution error");
+            exit(EXIT_FAILURE);
+        } else {
+            /* Parent process */
+            /* Wait for the child process to complete */
+            int childStatus;
+            waitpid(childPid, &childStatus, 0);
+
+            /* Check if the child process exited normally or due to a signal */
+            if (WIFEXITED(childStatus)) {
+                int exitStatus = WEXITSTATUS(childStatus);
+                (void)exitStatus;
+            } else if (WIFSIGNALED(childStatus))
+            {
+                int terminatingSignal = WTERMSIG(childStatus);
+                (void)terminatingSignal;
+            }
+        }
+    }
+
+    /* Free the allocated memory for the command */
+    free(command);
+
+    return (0);
 }
