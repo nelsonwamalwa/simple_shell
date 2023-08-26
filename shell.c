f7145b9712bc5cb25e97605a2678a15adbab845b
abel <abelamare3022@gmail.com>
2023-08-26 05:59:56 +0300

    f

--- shell.c
+++ shell.c
@@ -2,97 +2,83 @@
 
 void displayPrompt(void)
 {
-    if (isatty(STDIN_FILENO))
-    {
-        printf("Shelly> ");
-        fflush(stdout);
-    }
+	if (isatty(STDIN_FILENO))
+	{
+		printf("Shelly> ");
+		fflush(stdout);
+	}
+}
+ssize_t getCommand(char **command, size_t *commandSize)
+{
+	return (getline(command, commandSize, stdin));
 }
 
-/**
- * parse_input - Parse input into individual arguments.
- * @input: Input string to be parsed.
- * @args: Array to store parsed arguments.
- *
- * Return: Number of arguments parsed.
- */int main(void)
- {
-    char *command = NULL;
-    size_t commandSize = 0;
+void executeCommand(char **args)
+{
+	execvp(args[0], args);
+	perror("Command execution error");
+	exit(EXIT_FAILURE);
+}
 
-    while (1)
-    {
-        ssize_t bytesRead;
-        pid_t childPid;
+void waitForChildProcess(pid_t childPid)
+{
+	int childStatus;
 
-        int argCount = 0;
-        char *args[MAX_ARGS];
-        char *token;
+	waitpid(childPid, &childStatus, 0);
+	if (WIFEXITED(childStatus))
+	{
+		(void)WEXITSTATUS(childStatus);
+	}
+}
 
-        displayPrompt();
+int main(void)
+{
+	char *command = NULL;
+	size_t commandSize = 0;
 
-        /* Read user input using getline */
-        bytesRead = getline(&command, &commandSize, stdin);
+	while (1)
+	{
+		ssize_t bytesRead;
+		pid_t childPid;
+		char *args[MAX_ARGS];
+		int argCount = 0;
+		char *token;
 
-        if (bytesRead == -1) {
-            if (feof(stdin)) {
-                /* Handle end of file (Ctrl+D) */
-                break;
-            } else {
-                perror("Input error");
-                clearerr(stdin);  /* Clear the error state */
-                continue;         /* Continue to the next iteration */
-            }
-        }
-
-        /* Remove the newline character from the command */
-        if (command[bytesRead - 1] == '\n') {
-            command[bytesRead - 1] = '\0';
-        }
-
-        /* Tokenize the command */
-        token = strtok(command, " ");
-
-        while (token != NULL && argCount < MAX_ARGS - 1) {
-            args[argCount++] = token;
-            token = strtok(NULL, " ");
-        }
-        args[argCount] = NULL;
-
-        /* Fork a child process */
-        childPid = fork();
-
-        if (childPid < 0) {
-            /* Error forking */
-            perror("Fork error");
-        } else if (childPid == 0) {
-            /* Child process */
-            /* Execute the command using execvp */
-            execvp(args[0], args);
-
-            /* If execvp returns, an error occurred */
-            perror("Command execution error");
-            exit(EXIT_FAILURE);
-        } else {
-            /* Parent process */
-            /* Wait for the child process to complete */
-            int childStatus;
-            waitpid(childPid, &childStatus, 0);
-
-            /* Check if the child process exited normally or due to a signal */
-            if (WIFEXITED(childStatus)) {
-                int exitStatus = WEXITSTATUS(childStatus);
-                (void)exitStatus;
-            } else if (WIFSIGNALED(childStatus))
-            {
-                int terminatingSignal = WTERMSIG(childStatus);
-                (void)terminatingSignal;
-            }
-        }
-    }
-
-    /* Free the allocated memory for the command */
-    free(command);
-
-    return (0);
+		displayPrompt();
+		bytesRead = getCommand(&command, &commandSize);
+		if (bytesRead == -1)
+		{
+			if (feof(stdin))
+				break;
+			perror("Input error");
+			clearerr(stdin);
+			continue;
+		}
+		if (command[bytesRead - 1] == '\n')
+		{
+			command[bytesRead - 1] = '\0';
+		}
+		token = strtok(command, " ");
+		while (token != NULL && argCount < MAX_ARGS - 1)
+		{
+			args[argCount++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[argCount] = NULL;
+		childPid = fork();
+		if (childPid < 0)
+		{
+			perror("Fork error");
+		}
+		else if (childPid == 0)
+		{
+			executeCommand(args);
+		}
+		else
+		{
+			waitForChildProcess(childPid);
+		}
+	}
+	free(command);
+	return (0);
 }
